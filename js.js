d04d8620e8ee36201d7a44607e91b94a19034adf
Kartik <kartikmahar5@gmail.com>
2024-10-17 08:45:08 +0530

    jscode

---
+++ js.js
@@ -0,0 +1,17 @@
+//what is array?
+//araay is a collection of same or different datatype stored in a contiguos form.
+
+let a=["Yuvraj","Abhishek","Kailash","Kartik",6,8998,56,89]
+console.log(a.splice(2,2))
+console.log(a.slice(1))
+// let a2=[3,5,6,8,8,9,6,54]
+// // let final=(a.concat(a,a2))
+// console.log(a.concat(a,a2))
+// console.log(a[3])
+// console.log(a.length)
+// a.push("kailash")
+// a.pop("kailash")
+// // a.shift("kailash")
+// console.log(a)
+// console.log(a.toString())
+// console.log(a.join("%"))
